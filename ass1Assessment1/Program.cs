d5e1214f02d3bc2488032252fa1b9d5989b4b5e2
Bhekani Masinga <bhekani.masinga@nagarro.com>
2023-07-08 18:35:17 +0200

    first commit

---
+++ ass1Assessment1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign1_Assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            string input = Console.ReadLine();
+            int result = int.Parse(input);
+            Console.WriteLine("Parsed integer: " + result);
+
+            Console.WriteLine("Enter an integer: ");
+            string inpu = Console.ReadLine();
+            int resul = Convert.ToInt32(inpu);
+            Console.WriteLine("Converted integer: " + resul);
+
+            Console.WriteLine("Enter an integer: ");
+            string inpt = Console.ReadLine();
+            int reslt;
+            if (int.TryParse(inpt, out reslt))
+            {
+                Console.WriteLine("Try Parsed integer: " + reslt);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+        }
+    }
+}
